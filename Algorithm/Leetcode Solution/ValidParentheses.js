9b2d18874039344e6e5f71c42500a027682246e6
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-20 07:49:52 +0530

    Valid Parenthesis Solution added

---
+++ Algorithm/Leetcode Solution/ValidParentheses.js
@@ -0,0 +1,56 @@
+/*      -------------------------------
+        |    FUNCTIONAL PROGRAMMING   |
+        -------------------------------
+  Auther : Sonu Kumar (B.tech 2nd Year in 2020-2021)
+
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var op = "({[";
+var cl = ")}]";
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s.length % 2 != 0) return false;
+  var stack = [];
+  for (var i = 0; i < s.length; ++i) {
+    if (op.indexOf(s[i]) >= 0) stack.push(s[i]);
+    else if (cl.indexOf(s[i]) >= 0) {
+      var res = pop(stack, s[i]);
+      if (res) continue;
+      else return false;
+    }
+  }
+  if (stack.length == 0) return true;
+  else return false;
+};
+
+var pop = function (stack, c) {
+  var poped = stack.pop();
+  if (op.indexOf(poped) >= 0) {
+    if (poped == closeToOpen(c)) return true;
+    else return false;
+  } else if (cl.indexOf(poped) >= 0) {
+    return pop(stack, poped);
+  }
+};
+
+var closeToOpen = function (s) {
+  if (s === ")") {
+    return "(";
+  }
+  if (s === "}") {
+    return "{";
+  }
+  if (s === "]") {
+    return "[";
+  }
+};
+
+console.log(isValid("{([)}]"));
+console.log(isValid("{([])}"));
