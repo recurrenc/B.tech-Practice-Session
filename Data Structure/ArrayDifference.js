a711d1a8b106182325ba3c25a65197f03916c39e
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-20 08:18:48 +0530

    Array Difference Algorithem Added

---
+++ Data Structure/ArrayDifference.js
@@ -0,0 +1,26 @@
+/*      -------------------------------
+        |    FUNCTIONAL PROGRAMMING   |
+        -------------------------------
+  Auther : Sonu Kumar (B.tech 2nd Year in 2020-2021)
+
+*/
+
+function sym(...args) {
+  return [...args.reduce(reducer, new Set())];
+}
+
+function reducer(result, arr) {
+  const compare = new Set(arr);
+  console.log(arr);
+  console.log(compare);
+  for (let val of compare) {
+    if (result.has(val)) {
+      result.delete(val);
+    } else {
+      result.add(val);
+    }
+  }
+  return result;
+}
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
