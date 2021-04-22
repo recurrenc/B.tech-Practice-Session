3d41e7307ac95f55c890177f3f4df16e0c24ee04
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-22 19:34:54 +0530

    Cut The Stick Question Solve

---
+++ Algorithm/Hackerrank/Problem Solving/cutTheSticks.js
@@ -0,0 +1,18 @@
+function cutTheSticks(arr) {
+  let res = [];
+  arr.sort((a, b) => a - b);
+
+  let set = new Set(arr);
+
+  res.push(arr.length);
+  let itr = set.values();
+  for (let i = 0; i < set.size; i++) {
+    let temp = arr.length - 1 - arr.lastIndexOf(itr.next().value);
+    if (temp) res.push(temp);
+  }
+
+  return res;
+}
+
+let arr = [5, 4, 4, 2, 2, 8];
+console.log(cutTheSticks(arr));
