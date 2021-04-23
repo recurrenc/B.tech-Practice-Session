0c49615ee4878824de312e5199831e584f967f85
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-23 11:36:36 +0530

    Equalize the  Array Solution added

--- Algorithm/Hackerrank/Problem Solving/EqualizetheArray.js
+++ Algorithm/Hackerrank/Problem Solving/EqualizetheArray.js
@@ -1,3 +1,18 @@
 let arr = [5, 6, 6, 4, 6, 5, 6, 7];
 // Complete the equalizeArray function below.
-function equalizeArray(arr) {}
+function equalizeArray(arr) {
+  let res = [];
+
+  let temp = new Set(arr);
+  let itr = temp.values();
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < temp.size; i++) {
+    let v = itr.next().value;
+    res.push(arr.lastIndexOf(v) - arr.indexOf(v) + 1);
+  }
+  res.sort((a, b) => b - a);
+
+  return arr.length - res[0];
+}
+
+console.log(equalizeArray(arr));
