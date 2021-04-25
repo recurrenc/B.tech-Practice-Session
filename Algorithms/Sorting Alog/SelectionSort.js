6d6267b69e5cca90f108b5bd5ae0614d4ceecee8
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-25 16:10:21 +0530

    Selection Sort Algorithm

---
+++ Algorithms/Sorting Alog/SelectionSort.js
@@ -0,0 +1,30 @@
+/*      -----------------------------------------
+       |        Selection Sort Algorithem        |
+        -----------------------------------------
+  Auther : Sonu Kumar (B.tech 2nd Year in 2020-2021)
+
+*/
+
+const Swap = (list, a, b) => {
+  let temp = list[a];
+  list[a] = list[b];
+  list[b] = temp;
+  return list;
+};
+
+const SelectionSort = (list) => {
+  for (let i = 0; i < list.length; i++) {
+    let min = list[i];
+    let minIndex = i;
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[j] < min) {
+        min = list[j];
+        minIndex = j;
+      }
+    }
+    Swap(list, i, minIndex);
+  }
+  return list;
+};
+
+console.log(SelectionSort([5, 8, 99, 123, 3, 6, 8, 55]));
