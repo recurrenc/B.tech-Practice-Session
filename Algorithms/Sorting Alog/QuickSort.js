41a386f103b646a43e6d6a3b5ba96a5a6eac40ab
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-26 20:20:11 +0530

    Rearrange

--- Algorithms/Sorting Alog/QuickSort.js
+++ Algorithms/Sorting Alog/QuickSort.js
@@ -3,7 +3,7 @@
         -----------------------------------------
   Auther : Sonu Kumar (B.tech 2nd Year in 2020-2021)
 
-  What is Quick sort?
+What is Quick sort?
 Quick sort follows Divide and Conquer algorithm. It is dividing elements in to smaller parts based on some condition and performing the sort operations on those divided smaller parts. Hence, it works well for large datasets. So, here are the steps how Quick sort works in simple words.
 
 1.  First select an element which is to be called as pivot element.
@@ -40,20 +40,17 @@ const partition = (items, left, right) => {
 const quickSort = (items, left, right) => {
   let index;
   if (items.length > 1) {
-    index = partition(items, left, right); //index returned from partition
+    index = partition(items, left, right);
     if (left < index - 1) {
-      //more elements on the left side of the pivot
       quickSort(items, left, index - 1);
     }
     if (index < right) {
-      //more elements on the right side of the pivot
       quickSort(items, index, right);
     }
   }
   return items;
 };
-// first call to quick sort
 
 let items = [55, 4, 32, 23, 88];
 let sortedArray = quickSort(items, 0, items.length - 1);
-console.log(sortedArray); //prints [2,3,5,6,7,9]
+console.log(sortedArray);
