46c9a0d864a41c72fc567207c39ee067c4cd9b06
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-27 18:23:00 +0530

    Bigger is Greater Solution

---
+++ Hackerrank/Problem Solving/BiggerisGreater.js
@@ -0,0 +1,30 @@
+function biggerIsGreater(w) {
+  let vals = w.split("");
+
+  let largestI = -1;
+  for (let i = 0; i < vals.length - 1; i++) {
+    if (vals[i] < vals[i + 1]) {
+      largestI = i;
+    }
+  }
+  if (largestI === -1) {
+    return "no answer";
+  }
+  let largestJ = -1;
+  for (let j = 0; j < vals.length; j++) {
+    if (vals[largestI] < vals[j]) {
+      largestJ = j;
+    }
+  }
+  let temp = vals[largestI];
+  vals[largestI] = vals[largestJ];
+  vals[largestJ] = temp;
+
+  let endArray = vals.splice(largestI + 1);
+  endArray.reverse();
+  vals = vals.concat(endArray);
+
+  return vals.join("");
+}
+
+console.log(biggerIsGreater("abcd"));
