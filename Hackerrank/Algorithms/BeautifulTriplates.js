e432195cd0578ddc2c88fb3f7a34e866d6889ce7
Sonu Kumar <sonu.patna0808@gmail.com>
2021-04-29 06:51:31 +0530

    Hackerrank BeautifulTriplates Templet Setup Still need to solve  the problem

---
+++ Hackerrank/Algorithms/BeautifulTriplates.js
@@ -0,0 +1,15 @@
+/*
+ * Complete the 'beautifulTriplets' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+function beautifulTriplets(d, arr) {
+  // Write your code here
+  console.log(d + " " + arr[2]);
+}
+
+console.log(beautifulTriplets(3, [1, 2, 4, 5, 7, 8, 10]));
