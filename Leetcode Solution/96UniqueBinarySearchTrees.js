011068cb2eef50f9945687848875cd9f4df9090f
SonuKumar81800 <sonu.patna0808@gmail.com>
2021-11-08 13:10:50 +0530

    optimize 96UniqueBST

--- Leetcode Solution/96UniqueBinarySearchTrees.js
+++ Leetcode Solution/96UniqueBinarySearchTrees.js
@@ -2,12 +2,10 @@ function countBST(n) {
   let k = n;
   n = 2 * n;
   let res = 1;
-  if (k > n - k) k = n - k;
   for (let i = 0; i < k; ++i) {
-    res *= n - i;
-    res /= i + 1;
+    res *= (n - i) / (i + 1);
   }
   return res / (k + 1);
 }
 
-console.log(countBST(4));
+console.log(countBST(5));
